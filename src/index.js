6b0795d5efe45ed4948e1472bee3281e7f889ede
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-01-10 18:25:36 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import _ from 'lodash'
+// import { NAME, NiJou } from './utilities'
+// import * as utilities from './utilities'
+// import { NAME as NAME_OF_HAM } from './utilities'
+// import Lion from './utilities'
+import Tiger from './utilities'
+
+// console.log(NiJou(2));
+// console.log(NAME);
+// console.log(utilities.NAME);
+// console.log(NAME_OF_HAM);
+console.log(Tiger.say());
 
 function component() {
   const element = document.createElement('div');
