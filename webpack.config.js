f20e1ca36e805d0598d99bc8207ccffc6e66eba4
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-01-11 17:29:47 +0900

    implement babel-loader and react development

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 const outpustPath = path.resolve(__dirname, 'dist')
 
 module.exports = {
@@ -9,6 +10,11 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
       {
         test: /\.css$/,
         use: [
@@ -31,10 +37,20 @@ module.exports = {
           limit: 2048,
           name: './images/[name].[ext]'
         }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
       }
     ]
   },
   devServer: {
     contentBase: outpustPath
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ]
 }
